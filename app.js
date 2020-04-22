bcb481940d6759cd7d15251478216d0364fb4c31
David Orson <onlyorson@outlook.com>
2020-04-22 22:24:19 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,4 @@
+"use strict";
+var nav = document.querySelector("#nav");
+var li = document.createElement("li");
+nav.appendChild(li);
---
+++ app.ts
@@ -0,0 +1,5 @@
+const nav = document.querySelector("#nav");
+
+const li = document.createElement("li");
+
+nav.appendChild(li);
